3ca27cd4b34b686ad5c3eae7d62e83e24df8cc39
imaizumi <kit.imaizumi.dev@gmail.com>
2025-05-18 18:51:29 +0900

    clean:utilの分離

--- lib/features/goals/repository/goal_repository.dart
+++ lib/features/goals/repository/goal_repository.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../utils/goal_mapper.dart';
 import '../model/goal/goal.dart';
 import '../model/tag/tag.dart';
 
@@ -26,7 +27,7 @@ class GoalRepository {
   }
   /// 🔼 ゴールを追加
   Future<void> addGoal(String uid, Goal goal) async {
-    final data = _goalToFirestoreData(goal);
+    final data = goalToFirestoreData(goal);
     await _firestore
         .collection('users')
         .doc(uid)
@@ -60,58 +61,13 @@ class GoalRepository {
 
   /// 🛠 ゴールを更新
   Future<void> updateGoal(String uid, Goal goal) async {
-    final data = _goalToFirestoreData(goal);
+    final data = goalToFirestoreData(goal);
     await _firestore
         .collection('users')
         .doc(uid)
         .collection('goals')
         .doc(goal.id)
         .update(data);
-  }
-
-  /// 🧩 Goal の変換処理
-  Goal goalFromFirestore(String id, Map<String, dynamic> data) {
-    return Goal(
-      id: id,
-      title: data['title'] as String? ?? '',
-      detail: data['detail'] as String?,
-      deadline: (data['deadline'] as Timestamp?)?.toDate(),
-      done: data['done'] as bool? ?? false,
-      tags: (data['tags'] as List<dynamic>? ?? []).map((tagData) {
-        final map = tagData as Map<String, dynamic>;
-        return Tag(
-          id: map['id'] as String? ?? '',
-          name: map['name'] as String? ?? '',
-        );
-      }).toList(),
-      tasks: (data['tasks'] as List<dynamic>? ?? []).map((taskData) {
-        final map = taskData as Map<String, dynamic>;
-        return Task(
-          id: map['id'] as String? ?? '',
-          title: map['title'] as String? ?? '',
-          deadline: (map['deadline'] as Timestamp?)?.toDate(),
-          done: map['done'] as bool? ?? false,
-        );
-      }).toList(),
-    );
-  }
-
-  /// 🔃 Goal → Firestore 形式
-  Map<String, dynamic> _goalToFirestoreData(Goal goal) {
-    return {
-      'id': goal.id,
-      'title': goal.title,
-      'detail': goal.detail,
-      'deadline': goal.deadline != null ? Timestamp.fromDate(goal.deadline!) : null,
-      'done': goal.done,
-      'tags': goal.tags.map((tag) => tag.toJson()).toList(),
-      'tasks': goal.tasks.map((task) => {
-        'id': task.id,
-        'title': task.title,
-        'deadline': task.deadline != null ? Timestamp.fromDate(task.deadline!) : null,
-        'done': task.done,
-      }).toList(),
-    };
   }
 }
 
